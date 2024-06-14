fedb4b32ac846789cdbac6d9889c52ac80b77243
Larry Robles <134145081+Larryrzv@users.noreply.github.com>
2024-06-14 12:11:12 -0400

    Add i18n and fixing bugs
    Se agrega el "i18n" para soporte de la internacionalizacion. Se tienen la traduccion del ingles para toda la pagina. Se añadieron los archivos y carpetas necesarias(components/i18n/).
    
    Se arreglaron algunos bugs visuales para la visualizacion en pantallas reducidas.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://portfolio-larryrzv.netlify.app",
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      "es" : "en"
+    }
+  }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,131 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "title": "Portfolio de Larry Robles - Desarrollador y Programador Web Junior",
+    "description":
+      "Contrata a Larry Robles para formar parte de tu equipo, aligerara las cargas de los trabajos sencillos, compartira de su nueva energia al grupo de Desarrollo y no le temera a aprender nuevas herramientas y tecnologias en el proceso",
+    "nav.home": "inicio",
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.about": "Sobre mi",
+    "lay.greetings": "Holis, soy LarryRZV",
+    "lay.badge": "Disponible para trabajar",
+    "lay.span-1": "1 año de experiencia como ",
+    "lay.span-2": "Desarrollador Junior Fullstack.",
+    "lay.span-3": "De Bolivar, Venezuela.",
+    "lay.span-4":
+      "Especializado en el Desarrollo web Front-end con Conocimientos en Backend.",
+    "sect.cont-1": "Contactame",
+    "exp.span-1": "Asistente de Técnico en ",
+    "exp.span-2": "e Electronica ",
+    "exp.span-3": "Informatica",
+    "exp.time": "2020-2021 (1 Año de Experiencia)",
+    "exp.p-1": "Asistencia en reparación y exámenes básicos en ",
+    "exp.span-4": "hardware y ",
+    "exp.span-5": "software",
+    "exp.span-6": "(Smartphone, laptop, Computador, etc).",
+    "exp.p-2": "Recepcionista y atención al cliente.",
+
+    "proj.title-1": "Pagina de prueba: Electromoriz-web",
+    "proj.time-1": "Oct. 2023 - Abr. 2024 (7 meses)",
+    "proj.descrip-1":
+      "Una sencilla página estática que simula ser la página publicitaria de un técnico electromotriz",
+    "proj.title-2": "Aplicacion web: Card-Jitsu",
+    "proj.time-2": "Ago. 2023 - Abr. 2024 (9 meses)",
+    "proj.descrip-2":
+      "Proyecto de curso, pagina dinamica inspiradada en el juego de cartas Card Jitsu.",
+    "proj.button-1": "Sitio web",
+    "proj.button-2": "Mira el codigo",
+
+    "about.p-1":
+      "¡Hola! Mi nombre es Larry Robles y soy un estudiante en la carrera de Educación. Hace aproximadamente un año, descubrí mi pasión por la programación y desde entonces, me he ",
+    "about.strong-1":
+      "convertido en un Programador Junior. Por ahora estoy enfocandome en el desarrollo web tanto de paginas como de aplicaciones, ",
+    "about.p-2":
+      "pero planeo en un futuro incursionarme en el desarrollo de videojuegos.",
+    "about.p-3":
+      "Durante mi entrada en el mundo de la programación web, he trabajado en varios proyectos. Uno de los que más me enorgullece es una ",
+    "about.strong-2":
+      "aplicación web: una página dinámica donde puedes jugar contra la computadora o incluso contra ¡¡una persona real!!.",
+    "about.p-4":
+      "Aunque soy nuevo en este campo, no permitiré que el miedo me detenga. ",
+    "about.strong-3":
+      "Estoy decidido a alcanzar mis metas y seguir progresando.",
+    "about.p-5": "Si te ha parecido interesante mis",
+    "about.span-1": "capacidades y perfil para algun trabajo, ",
+    "about.p-6": "estaria ",
+    "about.strong-4": "encantado de poder comunicarme.",
+    "about.p-7":
+      "Aqui estan mis redes sociales y correos personales por los cuales puedes ",
+    "about.strong-5": "contactarme.",
+
+    "footer.span-1": "Algunos todos los derechos reservador",
+  },
+
+  en: {
+    "title": "Larry Robles Portfolio - Junior Web Developer and Programmer",
+    "description":
+      "Hire Larry Robles to be part of your team, he will lighten the loads of simple jobs, share his new energy with the Development group and will not be afraid to learn new tools and technologies in the process",
+    "nav.home": "Home",
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.about": "About me",
+    "lay.greetings": "Hi, I'm LarryRZV",
+    "lay.badge": "Available for work",
+    "lay.span-1": "1 year of experience as a ",
+    "lay.span-2": "Junior Fullstack Developer.",
+    "lay.span-3": "From Bolivar, Venezuela.",
+    "lay.span-4":
+      "Specialized in Front-end web development with knowledge in Backend.",
+    "sect.cont-1": "Contact me",
+    "exp.span-1": "Electronics and  ",
+    "exp.span-2": "IT",
+    "exp.span-3": "Technician Assistant",
+    "exp.time": "2020-2021 (1 Year of Experience)",
+    "exp.p-1": "Repair assistance and basic examinations on ",
+    "exp.span-4": "hardware and ",
+    "exp.span-5": "software",
+    "exp.span-6": "(Smartphone, laptop, cpu, etc.)",
+    "exp.p-2": "Receptionist and customer service.",
+
+    "proj.title-1": "Demo page: Electromoriz-web",
+    "proj.time-1": "Oct. 2023 - Apr. 2024 (7 months)",
+    "proj.descrip-1":
+      "A simple static page that simulates the advertising page of an electromotive technician.",
+    "proj.title-2": "Web application: Card-Jitsu",
+    "proj.time-2": "Aug. 2023 - Apr 2024 (9 months)",
+    "proj.descrip-2":
+      "Course project, dynamic page inspired by the card game Card Jitsu.",
+    "proj.button-1": "Web site",
+    "proj.button-2": "View the code",
+
+    "about.p-1":
+      "Hello! My name is Larry Robles and I am a student majoring in Education. About a year ago, I discovered my passion for programming and since then, I have become a ",
+    "about.strong-1":
+      "Junior Programmer. For now I'm focusing on web development for both web pages and applications, ",
+    "about.p-2":
+      "but I plan to dabble in video game development in the future.",
+    "about.p-3":
+      "During my entry into the world of web programming, I have worked on several projects. One of the ones that I am most proud of is ",
+    "about.strong-2":
+      "a web application: a dynamic page where you can play against the computer or even against a real person!!!.",
+    "about.p-4":
+      "Although I am new in this field, I will not let fear stop me. ",
+    "about.strong-3": "I am determined to reach my goals and keep progressing.",
+    "about.p-5": "If you have found my ",
+    "about.span-1": "skills and profile interesting for any job, ",
+    "about.p-6": "I would be ",
+    "about.strong-4": "happy to communicate with you.",
+    "about.p-7":
+      "Here are my social networks and personal emails through which you can ",
+    "about.strong-5": "contact me.",
+
+    "footer.span-1": "Some all rights reserved",
+  },
+} as const;
